b2910e7319427c15ebcae809b42e25fcf4c13e33
Roccom829 <roccom829@gmail.com>
2021-10-06 18:51:15 -0400

    Update script.js

--- Labs/Lab 5/Submission/script.js
+++ Labs/Lab 5/Submission/script.js
@@ -1,7 +1,6 @@
 document.querySelector("h1").innerHTML = "Hello";
 
-document.getElementById("description").innerHTML = "Rocco";
+document.querySelector("#description").innerHTML = "Rocco";
 
-document.getElementById("bossy").innerHTML = "Can you do something for me please?";
+document.querySelector("#bossy").innerHTML = "Can you do something for me please?";
 
-document.querySelector("body").innerHTML = document.bgcolor = "gray";
