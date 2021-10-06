46e2263a8550f4a18151e0c21e1c5e50fe5d4469
Roccom829 <roccom829@gmail.com>
2021-10-06 18:32:50 -0400

    Create script.js

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").innerHTML = "Hello";
+
+document.getElementById("description").innerHTML = "Rocco";
+
+document.getElementById("bossy").innerHTML = "Can you do something for me please?";
+
+document.querySelector("body").innerHTML = document.bgcolor = "gray";
