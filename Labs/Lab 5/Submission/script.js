7ae8388ad32f6ae9ff26c8fbd4c367530d494b88
Roccom829 <roccom829@gmail.com>
2021-10-06 20:16:31 -0400

    Update script.js

--- Labs/Lab 5/Submission/script.js
+++ Labs/Lab 5/Submission/script.js
@@ -4,3 +4,11 @@ document.querySelector("#description").innerHTML = "Rocco";
 
 document.querySelector("#bossy").innerHTML = "Can you do something for me please?";
 
+var li = document.querySelectorAll("li");
+document.querySelector("body").style.backgroundColor = "Gray";
+
+li[0].style.color = "Blue";
+li[1].style.backgroundColor = "Pink";
+li[3].style.textTransform = "uppercase";
+li[4].className = "title";
+
