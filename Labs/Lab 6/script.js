338a647d0690c19d1e9aa2ae751ea7f8267ddf12
Roccom829 <roccom829@gmail.com>
2021-11-04 01:32:04 -0400

    Lab 6 Completed

---
+++ Labs/Lab 6/script.js
@@ -0,0 +1,16 @@
+function submitLoginData()
+{
+    var logEmail = document.querySelector("loginEmail").value;  
+    console.log(logEmail);
+
+    var logPass = document.querySelector("loginPass").value;  
+    console.log(logPass);
+}
+function submitNewData()
+{
+    var newEmail = document.querySelector("loginEmail").value;  
+    console.log(newEmail);
+
+    var newPass = document.querySelector("loginPass").value;  
+    console.log(newPass);
+}
